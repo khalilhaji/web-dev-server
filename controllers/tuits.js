80cd16bc57b8537d65ec5be708d3372cd4d9fd02
Khalil Haji <khalilalihaji@gmail.com>
2022-04-06 23:32:02 -0400

    initial commit

---
+++ controllers/tuits.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    _id: "123",
+    postedBy: { username: "NASA" },
+    tuit: "In 2021, our @NASAPersevere Mars rover landed",
+    handle: "nasa",
+    logoImage: "../../../images/js.png",
+
+    stats: {
+      comments: 123,
+      retuits: 234,
+      likes: 345,
+      dislikes: 0,
+    },
+  },
+  {
+    _id: "234",
+    postedBy: { username: "NASA" },
+    tuit: "Ingenuity helicopter takes flight.",
+    handle: "nasa",
+    logoImage: "../../../images/js.png",
+
+    stats: {
+      comments: 123,
+      retuits: 234,
+      likes: 345,
+      dislikes: 0,
+    },
+  },
+];
